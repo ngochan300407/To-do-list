c5ca8c8ccd1264067cd2796059e5eb0c644543b9
ngochan300407 <ngochan300407@gmail.com>
2026-01-12 10:47:38 +0700

    backend

--- backend/server.js
+++ backend/server.js
@@ -5,15 +5,12 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-// Fake database (lưu trong RAM)
 let notes = [];
 
-// Lấy danh sách note
 app.get("/api/notes", (req, res) => {
   res.json(notes);
 });
 
-// Thêm note
 app.post("/api/notes", (req, res) => {
   const { text, date } = req.body;
 
@@ -25,18 +22,15 @@ app.post("/api/notes", (req, res) => {
 
   notes.push(newNote);
 
-  // ⚠️ TRẢ VỀ NOTE VỪA TẠO
   res.json(newNote);
 });
 
-// Xoá note
 app.delete("/api/notes/:id", (req, res) => {
   const id = Number(req.params.id);
   notes = notes.filter((n) => n.id !== id);
   res.json({ ok: true });
 });
 
-// Chạy server
 app.listen(3000, () => {
   console.log("Backend chạy ở http://localhost:3000");
 });
